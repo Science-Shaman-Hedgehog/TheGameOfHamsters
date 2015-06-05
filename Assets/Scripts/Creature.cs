f52f9f959bfe05d5c6ec2b8a8cd95db00dc9da85
serkix <wolkodavx@mail.ru>
2015-06-05 20:32:30 +0300

    Управление
    ДОБАВЛЕННО:
    -Управление хомяком
    -Прыжок
    -Бег
    -Вправо
    -Влево

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent (typeof (Rigidbody2D))]
+public class Creature : HObject 
+{
+
+	public Rigidbody2D body;
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Creature
+{
+	[RangeAttribute(250, 1000)]
+	public float speed=250;
+	[RangeAttribute(1, 10)]
+	public float jump=1;
+	void FixedUpdate()
+	{
+		body.AddForce (new Vector2(Input.GetAxis ("Horizontal") * speed * Time.deltaTime,0));
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			body.AddForce(Vector2.up*jump,ForceMode2D.Impulse);
+		}
+	}
+}
