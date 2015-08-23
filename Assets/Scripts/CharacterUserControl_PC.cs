2eaa08bb11faf70719223e3ea4a478a966282b1e
Meglablandat <erikpetrov97@gmail.com>
2015-08-23 17:25:30 +0300

    Improved control
    [ADDED] New character control script (for PC)
    [REMOVED] Old control

---
+++ Assets/Scripts/CharacterUserControl_PC.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+[RequireComponent(typeof(PlatformerCharacter2D))]
+public class CharacterUserControl_PC : MonoBehaviour
+{
+    [SerializeField] private KeyCode _JumpKey;
+    [SerializeField] private KeyCode _CrouchKey;
+
+    private PlatformerCharacter2D _Character;
+    private bool _Jump;
+
+    private void Awake()
+    {
+        _Character = GetComponent<PlatformerCharacter2D>();
+    }
+
+    private void Update()
+    {
+        if (!_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed
+            _Jump = Input.GetKeyDown(_JumpKey);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        bool crouch = Input.GetKey(_CrouchKey);
+        float h = Input.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        _Character.Move(h, crouch, _Jump);
+        _Jump = false;
+    }
+}
